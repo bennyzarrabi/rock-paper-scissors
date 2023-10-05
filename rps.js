b6592f5803a5806e88d8626c1921b2311985816b
Benny Zarrabi <bennyz@surewest.net>
2023-10-05 15:19:29 -0700

    I created user input and made it case insensitive, and created the function to play the game

--- rps.js
+++ rps.js
@@ -1,9 +1,34 @@
-function getComputerChoice() {
-    const computerSelection = Math.floor(Math.random() * 3);
+const computerSelection = Math.floor(Math.random() * 3);
 
+const userInput = prompt("Choose rock, paper, or scissors: ");
+
+const normalizedInput = userInput.toLowerCase();
+
+const userInput = playerSelection;
+
+function getComputerChoice() {
+    
     const options = ["rock", "paper", "scissors"];
 
     return options[computerSelection];
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose, paper beats rock!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win, rock beats scissors!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose, scissors beats paper!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win, paper beats rock!";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose, rock beats scissors!";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win, scissors beats paper!";
+    } else {
+        return "You ties, try again!";
+    }
 }
 
 const choice = getComputerChoice();
